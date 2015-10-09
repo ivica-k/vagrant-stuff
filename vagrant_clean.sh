96213acaf64caa5673c67ded8e53aac53bb2fcd9
ivica-k <ivica.kolenkas@gmail.com>
2015-10-09 22:05:51 +0200

    Added "Development tools" in order to install VBox additions

--- vagrant_clean.sh
+++ vagrant_clean.sh
@@ -17,6 +17,9 @@ if [ "$EUID" -ne 0 ]
     chmod 0700 /home/vagrant/.ssh/
     chmod 0600 /home/vagrant/.ssh/authorized_keys
     chown -R vagrant:vagrant /home/vagrant/.ssh
+    
+    echo ":: Installing Development tools"
+    yum -y groupinstall "development tools" > /dev/null
 
     echo ":: Installing VirtualBox additions"
     mount /dev/sr0 /mnt
